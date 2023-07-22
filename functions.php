eebcd073bfcc73b809bbb2734759e739a17684b3
beckej13820 <beckej@alumni.lemoyne.edu>
2023-07-22 18:59:31 -0400

    Fix text outline on front page slider

--- functions.php
+++ functions.php
@@ -2,7 +2,7 @@
 /* enqueue script for parent theme stylesheeet */        
 function childtheme_parent_styles() {
  
- // enqueue style
+ // enqueue style 
  wp_enqueue_style( 'parent', get_template_directory_uri().'/style.css' );                       
 }
 add_action( 'wp_enqueue_scripts', 'childtheme_parent_styles');
