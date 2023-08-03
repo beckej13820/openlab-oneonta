d54ac3bfac19b9a9881a36b27081b925769ac4ae
beckej13820 <beckej@alumni.lemoyne.edu>
2023-08-03 12:50:43 -0400

    add help text for emails

--- js/register.js
+++ js/register.js
@@ -346,6 +346,22 @@
 				}
 
 			}
+
+            // Give Faculty and Staff help text for choosing the correct email address
+            // modified from city tech openlab
+            function set_email_helper( accountType ) {
+                var helper = '';
+    
+                if ( 'staff' === accountType ) {
+                    helper = 'Example: firstname.lastname@oneonta.edu';
+                } else if ( 'faculty' === accountType ) {
+                    helper = 'Example: firstname.lastname@oneonta.edu.';
+                }
+    
+                $('.email-requirements').fadeOut( function() {
+                    $(this).html( helper ).fadeIn();
+                } );
+            }
 		}
 	);
 
