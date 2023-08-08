a9976b77665992c02b521ad3a4e1899d090a6570
beckej13820 <beckej@alumni.lemoyne.edu>
2023-08-07 22:11:21 -0400

    redo functions.php based on openlab-bmcc

--- functions.php
+++ functions.php
@@ -1,17 +1,8 @@
 <?php
 /* enqueue script for parent theme stylesheeet */        
-function openlab_oneonta_enqueue_styles() {
-
- // enqueue style 
- wp_enqueue_style( 'child_style', get_template_directory_uri() . '/style.css', array( 'main-styles' ), filemtime(get_template_directory()) . '/style.css', false );                       
+function openlab_oneonta_enqueue_parent_styles() {
+    
+    wp_enqueue_style( 'parent', get_template_directory_uri().'/style.css' );                       
 }
 
-function openlab_oneonta_enqueue_scripts() {
- 
-    // enqueue script 
-    wp_enqueue_script( 'email_warning', get_template_directory_uri().'/js/email_helper.js' );                       
-   }
-
-   add_action( 'wp_enqueue_scripts', 'openlab_oneonta_enqueue_styles');
-   add_action( 'wp_enqueue_scripts', 'openlab_oneonta_enqueue_scripts');
-?>
+add_action( 'wp_enqueue_scripts', 'openlab_oneonta_enqueue_parent_styles');
