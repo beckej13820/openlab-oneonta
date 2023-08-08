50d612d6b954346712ff5b184c1d5524348d71f3
beckej13820 <beckej@alumni.lemoyne.edu>
2023-08-08 15:57:52 -0400

    add enqueue to email helper js

--- functions.php
+++ functions.php
@@ -14,4 +14,14 @@ function openlab_oneonta_enqueue_styles() {
 		$theme->get( 'Version' )
 	);
 }
+
+function openlab_oneonta_email_helper() {
+	wp_enqueue_script( 
+		'openlab_oneonta_email_helper' ,
+		get_stylesheet_directory_uri() . '/js/email_helper.js' ,
+		array( 'email_helper' )
+	);
+}
+
 add_action( 'wp_enqueue_scripts' , 'openlab_oneonta_enqueue_styles' );
+add_action( 'wp_enqueue_scripts' , 'openlab_oneonta_email_helper');
