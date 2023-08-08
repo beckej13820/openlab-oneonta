6ac7a87340eb4b1ae20bec97625d2b7d85d3d48b
beckej13820 <beckej@alumni.lemoyne.edu>
2023-08-07 22:33:43 -0400

    fix close

--- functions.php
+++ functions.php
@@ -5,3 +5,5 @@ add_action( 'wp_enqueue_scripts', function() {
 
 	wp_enqueue_script( 'openlab-oneonta-email-helper', get_stylesheet_directory_uri() . '/js/email_helper.js', array( 'openlab-oneonta-email-helper' ) );
 }
+);
+
